70bd18cce23781b7df1ac4a6c5f840971483c765
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-12-15 18:35:03 +0100

    Refactor app component and add tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
+import PlayersList from "./components/PlayersList/PlayersList";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playersArr: []
+    };
+  }
+
+  onScoreUpdate = (playerIndex, scoreToAdd) => {
+    this.setState({
+      playersArr: this.state.playersArr.map((player, index) => {
+        if (index === playerIndex) {
+          return { ...player, score: (player.score += scoreToAdd) };
+        }
+        return player;
+      })
+    });
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <PlayersList
+          players={this.state.playersArr}
+          onScoreUpdate={this.onScoreUpdate}
+        />
+      </div>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import PlayerList from "./components/PlayersList/PlayersList";
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+it("should update user score", () => {
+  const appComponent = shallow(<App />);
+  const playersArr = [
+    {
+      name: "Matt",
+      score: 7
+    }
+  ];
+
+  appComponent.setState({ playersArr });
+
+  const onScoreUpdate = appComponent.find(PlayerList).prop("onScoreUpdate");
+
+  onScoreUpdate(0, 7);
+
+  const playersArrAfterUpdate = appComponent.state().playersArr;
+
+  const firstPlayerScore = playersArrAfterUpdate[0].score;
+
+  expect(firstPlayerScore).toEqual(14);
 });
