ee4f82e0527c2dcce0068fc6c8d1fcc46a60d3af
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-12-15 16:15:52 +0100

    Add player component with tests

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Player.css";
+
+const Player = props => (
+  <li className="Player">
+    <span className="player__name">{props.name}</span>
+    <span className="player__score">{props.score}</span>
+    <span
+      className="player__btn--add"
+      onClick={() => props.onPlayerScoreChange(1)}
+    >
+      +
+    </span>
+    <span
+      className="player__btn--subtract"
+      onClick={() => props.onPlayerScoreChange(-1)}
+    >
+      -
+    </span>
+  </li>
+);
+
+export default Player;
